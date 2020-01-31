0ee3f4bbd478f283f556d7703af1d0b8267d146a
Montonen Sinja TTV15SP <sinjamontonen@kamk.fi>
2020-01-31 21:21:31 +0200

    scripts

---
+++ BugFixer2020/Assets/Scripts/Enemy.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public int hp;
+    public int damage;
+    public float speed;
+    Player player;
+
+    void Start()
+    {
+        player = FindObjectOfType<Player>();
+    }
+
+    int TakeDamage(int dmg)
+    {
+        hp -= dmg;
+        return hp;
+    }
+
+    void DealDamage()
+    {
+        player.TakeDamage(damage);
+    }
+}
