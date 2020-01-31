d41a698c6d2bdc373cdbda727e3f5e9657c59688
HeadHoffer <kim.1996hoffmann@gmail.com>
2020-01-31 22:59:27 +0200

    Fixed sprites

--- BugFixer2020/Assets/Scripts/Grid.cs
+++ BugFixer2020/Assets/Scripts/Grid.cs
@@ -8,9 +8,9 @@ public class Grid : MonoBehaviour
     private int height;
     private int[,] gridArray;
 
-    public Sprite image;
+    //public Sprite image;
 
-    public Grid(int width, int height)
+    public Grid(int width, int height, Sprite gridSprite)
     {
         this.width = width;
         this.height = height;
@@ -23,19 +23,19 @@ public class Grid : MonoBehaviour
         {
             for (int y = 0; y<gridArray.GetLength(1); y++)
             {
-                SpawnTile(x,y);
+                SpawnTile(x,y, gridSprite);
             }
         }
 
     }
 
-    private void SpawnTile(int x, int y)
+    private void SpawnTile(int x, int y, Sprite gridSprite)
     {
         GameObject tile = new GameObject("x: " + x + "y: " + y);
         tile.transform.position = new Vector3(x, y);
-        tile.AddComponent<SpriteRenderer>();
-        tile.GetComponent<SpriteRenderer>().sprite = image;
-        //sprite.sprite = image;
+        var sprite = tile.AddComponent<SpriteRenderer>();
+        //tile.GetComponent<SpriteRenderer>().sprite = image;
+        sprite.sprite = gridSprite;
         //sprite.color = new Color(0, 0, 0);
     }
 
--- BugFixer2020/Assets/Scripts/Testing.cs
+++ BugFixer2020/Assets/Scripts/Testing.cs
@@ -4,10 +4,12 @@ using UnityEngine;
 
 public class Testing : MonoBehaviour
 {
+    public Sprite gridSprite;
+
     // Start is called before the first frame update
     void Start()
     {
-        Grid grid = new Grid(20, 10);
+        Grid grid = new Grid(20, 10, gridSprite);
     }
 }
 
