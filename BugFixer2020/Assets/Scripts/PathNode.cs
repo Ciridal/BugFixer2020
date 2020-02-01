f40213443e7045e394c9f0003386200a53bbadf1
Montonen Sinja TTV15SP <sinjamontonen@kamk.fi>
2020-02-01 16:36:56 +0200

    stuff

---
+++ BugFixer2020/Assets/Scripts/PathNode.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PathNode 
+{
+    public bool walkable;
+    public Vector3 worldPos;
+
+    public PathNode(bool _walkable, Vector3 _worldPos)
+    {
+        walkable = _walkable;
+        worldPos = _worldPos;
+
+    }
+}
