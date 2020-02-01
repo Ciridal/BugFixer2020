77cc918e758df9ae01de7fc8f32f7c80cebee909
HeadHoffer <kim.1996hoffmann@gmail.com>
2020-02-01 13:21:52 +0200

    Random seed maybe and scene manager

---
+++ BugFixer2020/Assets/Scripts/Randomizer.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Randomizer : MonoBehaviour
+{
+    //public string input;
+    public float seed;
+
+    void Start()
+    {
+        //seed = Random.Range(0, input.GetHashCode());
+    }
+
+    void Awake()
+    {
+        DontDestroyOnLoad(this.gameObject);
+    }
+    
+    public void SetSeed(string input)
+    {
+        seed = Random.Range(0, input.GetHashCode());
+    }
+}
---
+++ BugFixer2020/Assets/Scripts/SceneManagement.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneManagement : MonoBehaviour
+{
+    public Scene ActiveScene;
+
+    void start()
+    {
+        ActiveScene = SceneManager.GetActiveScene();
+    }
+
+    void Awake()
+    {
+        DontDestroyOnLoad(this.gameObject);
+    }
+
+    public void LoadScene(string name)
+    {
+        SceneManager.LoadScene(name);
+        ActiveScene = SceneManager.GetActiveScene();
+    }
+}
