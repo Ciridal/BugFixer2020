9bdd885ae58eec7a6c51bc6dbb70e389911c4adb
HeadHoffer <kim.1996hoffmann@gmail.com>
2020-02-01 19:06:03 +0200

    Basic player and camera movement

---
+++ BugFixer2020/Assets/Scripts/CameraBehaviour.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CameraBehaviour : MonoBehaviour
+{
+    public GameObject player;
+    private float zValue = -10;
+
+    public float minZValue = -10;
+    public float maxZValue = -30;
+
+    void Start()
+    {
+        
+    }
+    
+    void Update()
+    {
+        if (Input.GetAxis("Mouse ScrollWheel") != 0f)
+            zValue += Input.GetAxis("Mouse ScrollWheel");
+
+        if (zValue >= minZValue)
+            zValue = minZValue;
+
+        if (zValue <= maxZValue)
+            zValue = maxZValue;
+
+    }
+
+    public void Move()
+    {
+        this.transform.position = new Vector3(player.transform.position.x, player.transform.position.y, zValue);
+    }
+}
---
+++ BugFixer2020/Assets/Scripts/TestPlayer.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestPlayer : MonoBehaviour
+{
+    public float nodeLength = 1;
+    public GameObject camera;
+
+    void Start()
+    {
+        this.transform.position += new Vector3(nodeLength, nodeLength) * .5f;
+    }
+    
+    void Update()
+    {
+        if (Input.GetKeyUp(KeyCode.W))
+            this.transform.position = new Vector3(this.transform.position.x, this.transform.position.y + nodeLength, this.transform.position.z);
+
+        if (Input.GetKeyUp(KeyCode.A))
+            this.transform.position = new Vector3(this.transform.position.x - nodeLength, this.transform.position.y, this.transform.position.z);
+
+        if (Input.GetKeyUp(KeyCode.S))
+            this.transform.position = new Vector3(this.transform.position.x, this.transform.position.y - nodeLength, this.transform.position.z);
+
+        if (Input.GetKeyUp(KeyCode.D))
+            this.transform.position = new Vector3(this.transform.position.x + nodeLength, this.transform.position.y, this.transform.position.z);
+
+        camera.GetComponent<CameraBehaviour>().Move();
+    }
+}
