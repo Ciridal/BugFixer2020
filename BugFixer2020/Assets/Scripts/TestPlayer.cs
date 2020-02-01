a3e6cd7543abd8354611144960fae43c401debfa
Montonen Sinja TTV15SP <sinjamontonen@kamk.fi>
2020-02-01 20:16:11 +0200

    bullets

--- BugFixer2020/Assets/Scripts/TestPlayer.cs
+++ BugFixer2020/Assets/Scripts/TestPlayer.cs
@@ -6,6 +6,12 @@ public class TestPlayer : MonoBehaviour
 {
     public float nodeLength = 1;
     public GameObject camera;
+    public int hp;
+    //public Sprite bullet;
+    public GameObject bulletPrefab;
+    public float bulletSpeed;
+    private float lastFire;
+    public float fireDelay;
 
     void Start()
     {
@@ -26,6 +32,28 @@ public class TestPlayer : MonoBehaviour
         if (Input.GetKeyUp(KeyCode.D))
             this.transform.position = new Vector3(this.transform.position.x + nodeLength, this.transform.position.y, this.transform.position.z);
 
+        float shootHor = Input.GetAxis("ShootHorizontal");
+        float shootVert = Input.GetAxis("ShootVertical");
+
+        if((shootHor != 0 || shootVert != 0) && Time.time > lastFire + fireDelay)
+        {
+            Shoot(shootHor, shootVert);
+            lastFire = Time.time;
+        }
+
         camera.GetComponent<CameraBehaviour>().Move();
+    }
+
+    int TakeDamage(int dmg)
+    {
+        hp -= dmg;
+        return hp;
+    }
+
+    void Shoot(float x, float y)
+    {
+        GameObject bullet = Instantiate(bulletPrefab, transform.position, transform.rotation) as GameObject;
+        bullet.AddComponent<Rigidbody2D>().gravityScale = 0;
+        bullet.GetComponent<Rigidbody2D>().velocity = new Vector3((x < 0) ? Mathf.Floor(x) * bulletSpeed : Mathf.Ceil(x) * bulletSpeed, (y < 0) ? Mathf.Floor(y) * bulletSpeed : Mathf.Ceil(y) * bulletSpeed);
     }
 }
